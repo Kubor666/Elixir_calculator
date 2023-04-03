32983d463e4811cc95c3914a21ac4367eb51c55a
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-03 20:52:41 +0100

    Implementation of very basic calculator features

--- lib/elixir_calculator.ex
+++ lib/elixir_calculator.ex
@@ -1,18 +1,29 @@
 defmodule ElixirCalculator do
-  @moduledoc """
-  Documentation for `ElixirCalculator`.
-  """
+  def mainmenu do
+    IO.puts("What you would like to do ?")
+    choice = IO.gets(" 1. Add
+  2. Substract
+  3. Multiplication
+  4. Division: ")
+    choose_math(choice)
+  end
 
-  @doc """
-  Hello world.
+  def choose_math(choice) do
+    x =
+      IO.gets("Provide 1st number: ")
+      |> String.trim()
+      |> String.to_integer()
 
-  ## Examples
+    y =
+      IO.gets("Provide 2nd number: ")
+      |> String.trim()
+      |> String.to_integer()
 
-      iex> ElixirCalculator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    case choice do
+      "1\n" -> ElixirCalculator.Maths.add(x, y)
+      "2\n" -> ElixirCalculator.Maths.substract(x, y)
+      "3\n" -> ElixirCalculator.Maths.multi(x, y)
+      "4\n" -> ElixirCalculator.Maths.divide(x, y)
+    end
   end
 end
---
+++ lib/maths.ex
@@ -0,0 +1,17 @@
+defmodule ElixirCalculator.Maths do
+  def add(x, y) do
+    x + y
+  end
+
+  def substract(x, y) do
+    x - y
+  end
+
+  def multi(x, y) do
+    x * y
+  end
+
+  def divide(x, y) do
+    x / y
+  end
+end
