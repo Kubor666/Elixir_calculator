d0d32208af5f9dc6e6117499991fb00904c41cdc
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-03 22:46:59 +0100

    Added handling floats and integers. Handling when user will provide something else than number

--- lib/elixir_calculator.ex
+++ lib/elixir_calculator.ex
@@ -1,6 +1,6 @@
 defmodule ElixirCalculator do
   def mainmenu do
-    IO.puts("What you would like to do ?")
+    IO.puts("Select number of desired action")
     choice = IO.gets(" 1. Add
   2. Substract
   3. Multiplication
@@ -9,21 +9,31 @@ defmodule ElixirCalculator do
   end
 
   def choose_math(choice) do
-    x =
-      IO.gets("Provide 1st number: ")
-      |> String.trim()
-      |> String.to_integer()
+    x = ask_for_number()
+    y = ask_for_number()
+    which_action(choice, x, y)
+  end
 
-    y =
-      IO.gets("Provide 2nd number: ")
-      |> String.trim()
-      |> String.to_integer()
-
+  defp which_action(choice, x, y) do
     case choice do
       "1\n" -> ElixirCalculator.Maths.add(x, y)
       "2\n" -> ElixirCalculator.Maths.substract(x, y)
       "3\n" -> ElixirCalculator.Maths.multi(x, y)
       "4\n" -> ElixirCalculator.Maths.divide(x, y)
+      _ -> IO.puts("Please provide valid number of an option")
+    end
+  end
+
+  defp ask_for_number() do
+    z =
+      IO.gets("Provide a number ")
+      |> Float.parse()
+
+    if z == :error do
+      IO.puts("Wrong value type!")
+      ask_for_number()
+    else
+      elem(z, 0)
     end
   end
 end
