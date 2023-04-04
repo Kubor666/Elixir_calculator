635871e3119f78a20f384240a6b5a2ba14605e64
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-04 18:51:12 +0100

    Implemented contiunation of using calculator after receving the result

--- lib/maths.ex
+++ lib/maths.ex
@@ -1,17 +1,48 @@
 defmodule ElixirCalculator.Maths do
   def add(x, y) do
-    x + y
+    result = x + y
+    display_result(result)
   end
 
   def substract(x, y) do
-    x - y
+    result = x - y
+    display_result(result)
   end
 
   def multi(x, y) do
-    x * y
+    result = x * y
+    display_result(result)
   end
 
   def divide(x, y) do
-    x / y
+    result = x / y
+    display_result(result)
+  end
+
+  defp display_result(result) do
+    IO.puts("Your result is:")
+    IO.puts(result)
+    continue()
+  end
+
+  defp continue() do
+    IO.puts("Would you like to do some more maths?")
+    choice = IO.gets("1. Yes
+  2. No:
+  ")
+    choosen_option(choice)
+  end
+
+  defp choosen_option("1\n") do
+    ElixirCalculator.mainmenu()
+  end
+
+  defp choosen_option("2\n") do
+    System.stop(0)
+  end
+
+  defp choosen_option(_result) do
+    IO.puts("Please provide valid option!")
+    continue()
   end
 end
