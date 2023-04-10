1aaefaff3ed345df974e7783d1504e7e460ee2a3
Jakub Hebda <hebdajakub2aizst@gmail.com>
2023-04-10 17:58:52 +0100

    Added Unit tests

--- test/elixir_calculator_test.exs
+++ test/elixir_calculator_test.exs
@@ -2,7 +2,19 @@ defmodule ElixirCalculatorTest do
   use ExUnit.Case
   doctest ElixirCalculator
 
-  test "greets the world" do
-    assert ElixirCalculator.hello() == :world
+  test "add numbers" do
+    assert ElixirCalculator.Maths.add(2, 2) == 4
+  end
+
+  test "substract numbers" do
+    assert ElixirCalculator.Maths.substract(2, 2) == 0
+  end
+
+  test "multiply numbers" do
+    assert ElixirCalculator.Maths.multi(2, 2) == 4
+  end
+
+  test "divide numbers" do
+    assert ElixirCalculator.Maths.divide(2, 2) == 1
   end
 end
